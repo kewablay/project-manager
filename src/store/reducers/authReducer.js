54a8a71026cb151a9da611841cc0e939471f367f
kewablay <kojokewajunior123@gmail.com>
2022-01-10 18:38:18 +0000

    done with signout authentication

--- src/store/reducers/authReducer.js
+++ src/store/reducers/authReducer.js
@@ -1,24 +1,27 @@
-const initState ={
-    authError: null 
-}
+const initState = {
+  authError: null,
+};
 
-const authReducer = (state=initState, action) => {
-    switch(action.type){
-        case "LOGIN_ERROR":
-            console.log("Login failed")
-            return {
-                ...state, 
-                authError: "Login failed"
-            }
-        case "LOGIN_SUCCESS": 
-            console.log("Login success")
-            return {
-                ...state, 
-                authError: null
-            }
-        default: 
-            return state
-    }
-}
+const authReducer = (state = initState, action) => {
+  switch (action.type) {
+    case "LOGIN_ERROR":
+      console.log("Login failed");
+      return {
+        ...state,
+        authError: "Login failed",
+      };
+    case "LOGIN_SUCCESS":
+      console.log("Login success");
+      return {
+        ...state,
+        authError: null,
+      };
+    case "SIGNOUT_SUCCESS":
+      console.log("Signout success");
+      return state; 
+    default:
+      return state;
+  }
+};
 
-export default authReducer
+export default authReducer;
